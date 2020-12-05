cf713d6ed06281ff9fcb0b4b83cb45607f82ec66
Tam Pham <tampham47@live.com>
2020-12-05 10:52:16 +0700

    convert the page into personal blog

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import { withPrefix } from 'gatsby'
+
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
-import './all.sass'
+
 import useSiteMetadata from './SiteMetadata'
-import { withPrefix } from 'gatsby'
+import './all.sass'
 
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata()
