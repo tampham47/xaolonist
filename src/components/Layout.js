28f027e8eb61077fab7c208a61f883ad478c5165
Tam <tampham47@live.com>
2020-12-21 17:49:01 +0700

    preload fonts

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,6 +13,8 @@ const TemplateWrapper = ({ children }) => {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
+        <link rel='preload' as='style' href='https://fonts.googleapis.com/css2?family=Merriweather:wght@300&display=swap' />
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Merriweather:wght@300&display=swap' />
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
